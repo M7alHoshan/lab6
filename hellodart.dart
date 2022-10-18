4595a91c25dd6d903aa82c0613a246684edd1688
441460583 <99913164+M7alHoshan@users.noreply.github.com>
2022-10-18 09:58:57 +0300

    Create hellodart.dart
    First file on dart

---
+++ hellodart.dart
@@ -0,0 +1,17 @@
+void main() {
+  print("Hello Class!");
+  print("hello Class Student");
+  String nk = "hello Class Student";
+  print(nk);
+  
+  bool x;
+  x = 1000 > 20;
+  print(x);
+  
+  String name = "Shaqra";
+  if (name.length != 0) {
+    print ("String not empty");
+  } else {
+    print("String is empty");
+  }   
+}   
